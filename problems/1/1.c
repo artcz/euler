ebba9969cf096c42dc41d972fcb84794f0a33f77
Artur Czepiel <czepiel.artur@gmail.com>
2017-03-23 20:16:48 +0100

    hello world2 -- simple c99 implementation

---
+++ problems/1/1.c
@@ -0,0 +1,27 @@
+//
+// To run:
+//      gcc 1.c -o 1_c -std=gnu99
+//      ./1_c
+//
+// Problem:
+//    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//    we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+//    Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+#include <stdio.h>
+
+int solve(int up_to) {
+    int sum = 0;
+    for(int i = 0; i < up_to; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    printf("%d", solve(1000));
+}
