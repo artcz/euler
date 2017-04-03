09d1977094088c6fc69b48e3a94505a2894c245b
Artur Czepiel <czepiel.artur@gmail.com>
2017-04-03 09:11:53 +0200

    P5 -- C version

---
+++ problems/5/5.c
@@ -0,0 +1,36 @@
+
+// Problem
+// description
+// goes
+// here
+
+
+#include <stdio.h>
+#include <time.h>
+
+#define FALSE 0
+#define TRUE 1
+
+
+int solve() {
+    for(int i=2520;;i++) {
+        int found=FALSE;
+        for(int j=2;j<=20;j++) {
+            if(i%j != 0) {
+                found = FALSE;
+                break;
+            }
+            found = TRUE;
+        }
+        if(found) return i;
+    }
+}
+
+
+int main() {
+    clock_t begin = clock();
+    int result = solve();
+    clock_t end = clock();
+    double time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%d [in %f seconds]", result, time);
+}
