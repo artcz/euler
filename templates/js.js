220427742b183f59eb1a209c4fbc647d8353022a
Artur Czepiel <czepiel.artur@gmail.com>
2017-04-10 12:32:54 +0200

    added js.js template

---
+++ templates/js.js
@@ -0,0 +1,15 @@
+//     node foo.js
+
+// Problem:
+// 
+
+solve = function() {
+    return 0
+}
+
+
+
+// unfortunately the accurracy seems to be only 1ms.
+console.time('solver');
+console.log(solve());
+console.timeEnd('solver');
