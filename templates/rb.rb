850acb75604ed0a3e9625448b955a5c152922d00
Artur Czepiel <czepiel.artur@gmail.com>
2017-04-13 10:49:34 +0200

    added rb.rb template

---
+++ templates/rb.rb
@@ -0,0 +1,16 @@
+# To run:
+#   ruby foo.rb
+#
+# Problem:
+# 
+
+
+def solve
+    result = 0
+end
+
+start = Time.now
+result = solve
+finish = Time.now
+
+puts "#{result} in #{finish - start} seconds"
