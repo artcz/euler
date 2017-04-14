594e5ec6b5b3cf1f3d0120e45bfb5c0d2bf4571d
Artur Czepiel <czepiel.artur@gmail.com>
2017-04-15 01:06:04 +0200

    first version of 7.c

---
+++ problems/7/7.c
@@ -0,0 +1,50 @@
+// gcc 7.c -std=c11 -lm
+//
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+// that the 6th prime is 13.
+//
+// What is the 10 001st prime number?
+
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+int solve() {
+    int prime_count = 2, number = 2, is_prime = FALSE;
+
+    while(1) {
+        // fun fact: going up to sqrt(number) takes about 0.02s
+        // going to j <= number takes much much much longer.
+        for(int j = 2; j <= sqrt(number); j++) {
+            if(number % j == 0) {
+                is_prime = FALSE;
+                break;
+            } else {
+                is_prime = TRUE;
+            }
+        }
+
+        if(is_prime) {
+            prime_count++;
+            if(prime_count == 10001) {
+                return number;
+            }
+        }
+
+        number += 1;
+    }
+}
+
+
+int main() {
+    clock_t begin = clock();
+    int result = solve();
+    clock_t end = clock();
+    double time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%d [in %f seconds]", result, time);
+}
