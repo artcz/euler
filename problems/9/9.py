f633f036b6ae3f30fc61c3790857dd816c3bceb2
Artur Czepiel <czepiel.artur@gmail.com>
2017-04-21 13:10:22 +0200

    9.py added oneliner

--- problems/9/9.py
+++ problems/9/9.py
@@ -15,8 +15,13 @@ import time
 
 
 def oneliner():
-    "TODO: write a oneliner"
-    pass
+
+    return [(a, b, c, a * b * c)
+            for c in range(5, 500)
+            for b in range(1, c)
+            for a in range(1, b)
+            if a + b + c == 1000 and a**2 + b**2 == c**2][0]
+
 
 
 def solve1():
